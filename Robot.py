5c6c58ccc3ce070fd524a8c81d390574e1881bff
Celeter <48249130+Celeter@users.noreply.github.com>
2020-09-18 21:31:33 +0800

    fix

--- Robot.py
+++ Robot.py
@@ -45,7 +45,7 @@ def convert2clash(arr):
         'proxy_names': []
     }
     for item in arr:
-        if item.get('ps') and item.get('add') and item.get('port') and item.get('id') and type(item.get('aid')) is int:
+        if item.get('ps') is None or item.get('add') is None or item.get('port') is None or item.get('id') is None and item.get('aid') is None:
             continue
         obj = {
             'name': item.get('ps'),
