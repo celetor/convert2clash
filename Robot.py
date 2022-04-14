ea2d0cca318d0bb5aff90a879602072b3fb26d19
celetor <celetor@github.com>
2022-04-14 12:15:37 +0800

    fix url

--- Robot.py
+++ Robot.py
@@ -343,7 +343,7 @@ if __name__ == '__main__':
     # 输出路径
     output_path = './output.yaml'
     # 规则策略
-    config_url = 'https://cdn.jsdelivr.net/gh/Celeter/convert2clash@main/config.yaml'
+    config_url = 'https://cdn.jsdelivr.net/gh/celetor/convert2clash@main/config.yaml'
     config_path = './config.yaml'
 
     if sub_url is None or sub_url == '':
